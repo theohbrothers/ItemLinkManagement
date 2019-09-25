222f9edac9da4983c0f8f0aafb95386470ef9aef
Joel Timothy Oh <joeltimothyoh@live.com>
2019-09-25 22:20:52 +0800

    Various improvements to Add-Link:
    Add trimming of extra spaces in specified strings
    Improve error handling
    And check for valid link types
    Fix check for existing link with matching link type, path, and value

--- src/ItemLinksManagement/Public/Add-Link.ps1
+++ src/ItemLinksManagement/Public/Add-Link.ps1
@@ -17,18 +17,31 @@ function Add-Link {
         [ValidateNotNullOrEmpty()]
         [switch]$Force
     )
-    "ItemType: '$($ItemType)', Path: '$($Path)', Value: '$($Value)'" | Write-Verbose
-    $item = Get-Item -Path $Path -ErrorAction SilentlyContinue
-    if ($item) {
-        if (!$item.LinkType) {
-            throw "Item '$Path' is not a SymbolicLink or Junction."
-        }
-        if ($item.Target -eq $Value) {
-            return
-        }
-        if ($item.LinkType -eq 'Junction') {        # New-Item -Force does not work for junctions
-            $item.Delete()                          # Remove-Item -Force and -Confirm:$false do not suppress confirmation for removal if items exists within symlink or junction target
+    begin {
+        $Path = $Path.Trim()
+        $ItemType = $ItemType.Trim()
+        $Value = $Value.Trim()
+    }process {
+        try {
+            if ($ItemType -notmatch '^(symbolicLink|junction)$') {
+                throw "The only valid item types are 'symbolicLink' or 'junction'."
+            }
+            "ItemType: '$($ItemType)', Path: '$($Path)', Value: '$($Value)'" | Write-Verbose
+            $item = Get-Item -Path $Path -ErrorAction SilentlyContinue
+            if ($item) {
+                if (!$item.LinkType) {
+                    throw "Item '$Path' is not a SymbolicLink or Junction."
+                }
+                if (($item.LinkType -eq $LinkType) -and ($item.Target -eq $Value)) {
+                    return
+                }
+                if ($item.LinkType -eq 'Junction') {        # New-Item -Force does not work for junctions
+                    $item.Delete()                          # Remove-Item -Force and -Confirm:$false do not suppress confirmation for removal if items exists within symlink or junction target
+                }
+            }
+            New-Item -Path $Path -ItemType $ItemType -Value $Value -Force:$Force
+        }catch {
+            $_ | Write-Error
         }
     }
-    New-Item -Path $Path -ItemType $ItemType -Value $Value -Force:$Force
 }
