5dc3af54afcffe54fc17398780797d815b04cc81
Joel Timothy Oh <joeltimothyoh@live.com>
2019-11-18 15:36:38 +0800

    Use camelcase for processed variables

--- src/ItemLinksManagement/Public/Add-Link.ps1
+++ src/ItemLinksManagement/Public/Add-Link.ps1
@@ -3,7 +3,7 @@ function Add-Link {
     Param (
         [Parameter(Mandatory=$true, ValueFromPipeline, ValueFromPipelineByPropertyName)]
         [ValidateNotNullOrEmpty()]
-        [string]$Path
+        [string]$path
         ,
         [Parameter(Mandatory=$true, ValueFromPipeline, ValueFromPipelineByPropertyName)]
         [ValidateSet("SymbolicLink", "Junction")]
@@ -11,37 +11,37 @@ function Add-Link {
         ,
         [Parameter(Mandatory=$true, ValueFromPipeline, ValueFromPipelineByPropertyName)]
         [ValidateNotNullOrEmpty()]
-        [string]$Value
+        [string]$value
         ,
         [Parameter(Mandatory=$false)]
         [switch]$Force
     )
     begin {
-        $Path = $PSBoundParameters['Path'].Trim()
-        $Value = $PSBoundParameters['Value'].Trim()
+        $path = $PSBoundParameters['Path'].Trim()
+        $value = $PSBoundParameters['Value'].Trim()
         if ($env:OS -eq 'Windows_NT') {
-            $Path = $Path -replace "/","\"
-            $Value = $Value -replace "/","\"
+            $path = $path -replace "/","\"
+            $value = $value -replace "/","\"
         }
     }process {
         try {
-            "ItemType: '$($PSBoundParameters['ItemType'])', Path: '$($Path)', Value: '$($Value)'" | Write-Verbose
-            $item = Get-Item -Path $Path -ErrorAction SilentlyContinue
+            "ItemType: '$($PSBoundParameters['ItemType'])', Path: '$($path)', Value: '$($value)'" | Write-Verbose
+            $item = Get-Item -Path $path -ErrorAction SilentlyContinue
             if ($item) {
                 if (!$item.LinkType) {
-                    throw "Existing item '$Path' is not a SymbolicLink or Junction."
+                    throw "Existing item '$path' is not a SymbolicLink or Junction."
                 }
-                if (($item.LinkType -eq $PSBoundParameters['ItemType']) -and ($item.Target -eq $Value)) {
-                    "Matching item '$Path' already exists. Skipping" | Write-Verbose
+                if (($item.LinkType -eq $PSBoundParameters['ItemType']) -and ($item.Target -eq $value)) {
+                    "Matching item '$path' already exists. Skipping" | Write-Verbose
                     return
                 }
                 if ($item.LinkType -ne $PSBoundParameters['ItemType'] -Or $item.LinkType -eq 'Junction') {      # New-Item -Force does not work for junctions, hence the need to remove the existing item
-                    "Removing existing junction item '$Path'" | Write-Verbose
+                    "Removing existing junction item '$path'" | Write-Verbose
                     $item.Delete()                                                                              # Remove-Item -Force and -Confirm:$false do not suppress confirmation for removal if items exists within symlink or junction target
                 }
             }
-            "Creating item '$Path'" | Write-Verbose
-            New-Item -Path $Path -ItemType $PSBoundParameters['ItemType'] -Value $Value -Force:$PSBoundParameters['Force']
+            "Creating item '$path'" | Write-Verbose
+            New-Item -Path $path -ItemType $PSBoundParameters['ItemType'] -Value $value -Force:$PSBoundParameters['Force']
         }catch {
             Write-Error -Exception $_.Exception -Message $_.Exception.Message -Category $_.CategoryInfo.Category -TargetObject $_.TargetObject
         }
