76072565f4c0c3c23a403a8acf27094b102ee377
Joel Timothy Oh <joeltimothyoh@live.com>
2020-03-10 14:14:50 +0800

    Remove use of $PSBoundParameters

--- src/ItemLinksManagement/Public/Add-Link.ps1
+++ src/ItemLinksManagement/Public/Add-Link.ps1
@@ -17,31 +17,31 @@ function Add-Link {
         [switch]$Force
     )
     begin {
-        $path = $PSBoundParameters['Path'].Trim()
-        $value = $PSBoundParameters['Value'].Trim()
+        $path = $Path.Trim()
+        $value = $Value.Trim()
         if ($env:OS -eq 'Windows_NT') {
             $path = $path -replace "/","\"
             $value = $value -replace "/","\"
         }
     }process {
         try {
-            "ItemType: '$($PSBoundParameters['ItemType'])', Path: '$($path)', Value: '$($value)'" | Write-Verbose
+            "ItemType: '$($ItemType)', Path: '$($path)', Value: '$($value)'" | Write-Verbose
             $item = Get-Item -Path $path -ErrorAction SilentlyContinue
             if ($item) {
                 if (!$item.LinkType) {
                     throw "Existing item '$path' is not a SymbolicLink or Junction."
                 }
-                if (($item.LinkType -eq $PSBoundParameters['ItemType']) -and ($item.Target -eq $value)) {
+                if (($item.LinkType -eq $ItemType) -and ($item.Target -eq $value)) {
                     "Matching item '$path' already exists. Skipping" | Write-Verbose
                     return
                 }
-                if ($item.LinkType -ne $PSBoundParameters['ItemType'] -Or $item.LinkType -eq 'Junction') {      # New-Item -Force does not work for junctions, hence the need to remove the existing item
+                if ($item.LinkType -ne $ItemType) {            # New-Item -Force does not work for junctions, hence the need to remove the existing item
                     "Removing existing junction item '$path'" | Write-Verbose
-                    $item.Delete()                                                                              # Remove-Item -Force and -Confirm:$false do not suppress confirmation for removal if items exists within symlink or junction target
+                    $item.Delete()                                                  # Remove-Item -Force and -Confirm:$false do not suppress confirmation for removal if items exists within symlink or junction target
                 }
             }
             "Creating item '$path'" | Write-Verbose
-            New-Item -Path $path -ItemType $PSBoundParameters['ItemType'] -Value $value -Force:$PSBoundParameters['Force']
+            New-Item -Path $path -ItemType $ItemType -Value $value -Force:$Force
         }catch {
             Write-Error -Exception $_.Exception -Message $_.Exception.Message -Category $_.CategoryInfo.Category -TargetObject $_.TargetObject
         }
