929a3e66c8f90dd22211a8bf96a9c05dff5a1ca3
Joel Timothy Oh <joeltimothyoh@live.com>
2021-08-01 20:55:31 +0800

    Add module manifest definition.ps1

---
+++ build/definitions/modulemanifest/definition.ps1
@@ -0,0 +1,55 @@
+# - Initial setup: Fill in the GUID value. Generate one by running the command 'New-GUID'. Then fill in all relevant details.
+# - Ensure all relevant details are updated prior to publishing each version of the module.
+# - To simulate generation of the manifest based on this definition, run the included development entrypoint script Invoke-PSModulePublisher.ps1.
+# - To publish the module, tag the associated commit and push the tag.
+
+@{
+    RootModule = 'ItemLinkManagement.psm1'
+    # ModuleVersion = ''                            # Value will be set for each publication based on the tag ref. Defaults to '0.0.0' in development environments and regular CI builds
+    GUID = '635fed29-1ca8-4e71-bd09-a22c2649fdc4'
+    Author = 'The Oh Brothers'
+    CompanyName = 'The Oh Brothers'
+    Copyright = '(c) 2021 The Oh Brothers'
+    Description = 'A powershell module for managing hardlinks, symlinks, and junctions.'
+    PowerShellVersion = '3.0'
+    # PowerShellHostName = ''
+    # PowerShellHostVersion = ''
+    # DotNetFrameworkVersion = ''
+    # CLRVersion = ''
+    # ProcessorArchitecture = ''
+    # RequiredModules = @()
+    # RequiredAssemblies = @()
+    # ScriptsToProcess = @()
+    # TypesToProcess = @()
+    # FormatsToProcess = @()
+    # NestedModules = @()
+    FunctionsToExport = @(
+        'Add-Link'
+    )
+    CmdletsToExport = @()
+    VariablesToExport = @()
+    AliasesToExport = @()
+    # DscResourcesToExport = @()
+    # ModuleList = @()
+    # FileList = @()
+    PrivateData = @{
+        # PSData = @{           # Properties within PSData will be correctly added to the manifest via Update-ModuleManifest without the PSData key. Leave the key commented out.
+            Tags = @(
+                'item'
+                'link'
+                'hardlink'
+                'junction'
+                'symboliclink'
+            )
+            LicenseUri = 'https://raw.githubusercontent.com/theohbrothers/ItemLinkManagement/master/LICENSE'
+            ProjectUri = 'https://github.com/theohbrothers/ItemLinkManagement'
+            # IconUri = ''
+            # ReleaseNotes = ''
+            # Prerelease = ''
+            # RequireLicenseAcceptance = $false
+            # ExternalModuleDependencies = @()
+        # }
+        # HelpInfoURI = ''
+        # DefaultCommandPrefix = ''
+    }
+}
